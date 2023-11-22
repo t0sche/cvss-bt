e4b352c5f45de6f65457b48026929198d15d011d
Stephen Shaffer <stephen@shaffer.systems>
2023-11-21 23:45:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# cvss-bt
+Enriching the NVD CVSS scores to include Temporal/Threat Metrics
+
+## Overview
+
+The Common Vulnerability Scoring System (CVSS) is an industry standard for assessing the severity of computer system security vulnerabilities. CVSS attempts to establish a measure of how severe a vulnerability is based on its attributes.
+
+The National Vulnerability Database includes CVSS Base scores in its catalog, but base scores are not enough to effectively prioritizie or contextualize vulnerabilities. In this repository I continuously enrich the CVSSv3.x score by using the Exploit Code Maturity (E) Temporal Metric.
+
+### Temporal Metric - Exploit Code Maturity (E)
+
+Source: https://www.first.org/cvss/v3.1/specification-document
+
+| Value | Description | CVE Present In |
+|---------------------------|-------------|-------------|
+| High (H)                  | Functional autonomous code exists, or no exploit is required (manual trigger) and details are widely available. Exploit code works in every situation, or is actively being delivered via an autonomous agent (such as a worm or virus). Network-connected systems are likely to encounter scanning or exploitation attempts. Exploit development has reached the level of reliable, widely available, easy-to-use automated tools. | CISA KEV, EPSS > Threshold |
+| Functional (F)            | Functional exploit code is available. The code works in most situations where the vulnerability exists. | Metasploit, Nucelei |
+| Proof-of-Concept (P)      | Proof-of-concept exploit code is available. The code might not work in all situations. | ExploitDB |
+| Unproven (U)              | No exploit code is available, or an exploit is theoretical. | CVE not present in any threat intelligence source above. |
+| Not Defined (X)           | Assigning this value to the metric will not influence the score. It means the user does not have enough information to assign a score. | We drop this value since we have information to assign a score. |
+
+
+
+## Features
+This repository continuously enriches and publishes CVSSv3.x Temporal Scores based on the following threat intelligence:
+
+- CISA KEV
+- EPSS
+- Metasploit
+- Nuclei
+- ExploitDB
+
+### Steps
+- Fetches CVSSv3.x scores from NVD every 6 hours
+- Fetches EPSS scores every morning
+- Calculates the Exploit Code Maturity (E) Metric when new data is found.
+- Provides a resulting CVSS-BT score for CVSSv3.x
+
+## To Do
+- CVSSv4 Threat Metrics
---
+++ code/enrich_nvd.py
@@ -0,0 +1,134 @@
+from urllib.request import urlopen
+from datetime import date
+import json
+import pandas as pd
+from cvss import CVSS3
+
+
+EPSS_CSV = f'data/epss/epss_scores-{date.today()}.csv'
+METASPLOIT_JSON = 'https://raw.githubusercontent.com/rapid7/metasploit-framework/master/db/modules_metadata_base.json'
+NUCLEI_JSON = 'https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/main/cves.json'
+EXPLOITDB_CSV = 'https://gitlab.com/exploit-database/exploitdb/-/raw/main/files_exploits.csv'
+KEV_JSON = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
+
+EPSS_THRESHOLD = 0.36
+"""
+36% is the threshold correlated to the F1 score of EPSSv3 model
+At ~37%, the CVE is very likely to have weaponized exploit code
+"""
+
+def enrich(df, epss_csv):
+    """
+    Enrich CVE data with EPSS, KEV, ExploitDB, Metasploit, and Nuclei data
+    """
+    #Load EPSS Data
+    epss_df = pd.read_csv(epss_csv, comment='#', compression='gzip')
+
+    #Load KEV Data
+    with urlopen(KEV_JSON) as response:
+        kev_json_data = json.loads(response.read())
+    kev_cve_list = []
+    for vuln in kev_json_data.get('vulnerabilities'):
+        kev_cve_list.append(vuln.get('cveID'))
+    kev_df = pd.DataFrame(kev_cve_list, columns=['cve'])
+    kev_df['cisa_kev'] = True
+
+    #Load ExploitDB
+    exploitdb_df = pd.read_csv(EXPLOITDB_CSV, usecols=['codes']).rename(columns={"codes": "cve"})
+    exploitdb_df.drop_duplicates(inplace=True)
+    exploitdb_df = exploitdb_df['cve'].str.extract(r"(CVE-\d{4}-\d{4,7})", expand=False).dropna().values
+    exploitdb_df = pd.DataFrame(exploitdb_df, columns = ['cve'])
+    exploitdb_df['exploitdb'] = True
+
+    #Load Metasploit
+    with urlopen(METASPLOIT_JSON) as response:
+        ms_json_data = json.loads(response.read())
+    ms_cve_list = []
+    for item in ms_json_data:
+        if 'references' in ms_json_data[item]:
+            cve_references = [ref for ref in ms_json_data[item]['references'] if ref.startswith('CVE-')]
+            ms_cve_list.extend(cve_references)
+    metasploit_df = pd.DataFrame(ms_cve_list, columns=['cve'])
+    metasploit_df['metasploit'] = True
+
+    #Load Nuclei
+    nuclei_df = pd.read_json(NUCLEI_JSON, lines=True)
+    nuclei_df.rename(columns={"ID": "cve"}, inplace=True)
+    nuclei_df = nuclei_df.drop(columns=['Info', 'file_path'])
+    nuclei_df['nuclei'] = True
+    
+    print('Mapping EPSS Data')
+    df = pd.merge(df, epss_df, on='cve', how='left').fillna(False)
+
+    print('Mapping KEV Data')
+    df = pd.merge(df, kev_df, on='cve', how='left').fillna(False)
+
+    print('Mapping ExploitDB Data')
+    df = pd.merge(df, exploitdb_df, on='cve', how='left').fillna(False)
+
+    print('Mapping Metasploit Data')
+    df = pd.merge(df, metasploit_df, on='cve', how='left').fillna(False)
+
+    print('Mapping Nuclei Data')
+    df = pd.merge(df, nuclei_df, on='cve', how='left').fillna(False)
+    
+    df = df.drop_duplicates(subset='cve')
+    return df
+
+
+def update_temporal_score(df, epss_threshold):
+    """
+    Update temporal score and severity based on exploit maturity
+    """
+
+    df['exploit_maturity'] = 'E:U'  # Default value
+
+    # First condition for 'E:H'
+    condition_eh = (df['cisa_kev']) | (df['epss'] >= epss_threshold) | (df['metasploit'])
+    # Next condition for 'E:F'
+    condition_ef = (~condition_eh) & (df['nuclei'])
+    # Last condition for 'E:P'
+    condition_ep = (~condition_eh) & (~condition_ef) & (df['exploitdb'])
+
+    df.loc[condition_eh, 'exploit_maturity'] = 'E:H'
+    df.loc[condition_ef, 'exploit_maturity'] = 'E:F'
+    df.loc[condition_ep, 'exploit_maturity'] = 'E:P'
+
+    # Update vector with exploit maturity
+    df['cvss-bt_vector'] = df['base_vector'] + '/' + df['exploit_maturity']
+
+    # Apply CVSS computation row-wise
+    def compute_cvss(row):
+        c = CVSS3(row['cvss-bt_vector'])
+        return c.temporal_score, c.severities()[1].upper()
+
+    # Extracting CVSS scores and severities
+    df[['cvss-bt_score', 'cvss-bt_severity']] = df.apply(compute_cvss, axis=1, result_type='expand')
+
+    return df
+
+
+nvd_df = pd.read_csv('data/nvd.csv')
+
+nvd_df = enrich(nvd_df, EPSS_CSV)
+nvd_df = update_temporal_score(nvd_df, EPSS_THRESHOLD)
+
+columns = [
+    'cve', 
+    'cvss-bt_score', 
+    'cvss-bt_severity', 
+    'cvss-bt_vector', 
+    'cvss_version', 
+    'base_score', 
+    'base_severity', 
+    'base_vector', 
+    'epss', 
+    'cisa_kev', 
+    'exploitdb', 
+    'metasploit', 
+    'nuclei'
+]
+
+cvss_bt_df = nvd_df[columns]
+
+cvss_bt_df.to_csv(f'cvss-bt-{date.today()}.csv', index=False)
