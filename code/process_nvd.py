e9058038600d71126adb099010879b71b6d19159
Stephen Shaffer <stephen.shaffer8@gmail.com>
2025-05-14 22:56:18 -0400

    Refactor process_nvd_files to improve vulnerability processing logic

--- code/process_nvd.py
+++ code/process_nvd.py
@@ -23,41 +23,40 @@ def process_nvd_files():
             for line in file:
                 data = json.loads(line)
                 vulnerabilities = data.get('CVE_Items', [])
-            print(f'CVEs in {file_path.name}:', len(vulnerabilities))
 
-            for entry in vulnerabilities:
-                if not entry['cve']['description']['description_data'][0]['value'].startswith('**'):
-                    cve = entry['cve']['CVE_data_meta']['ID']
-                    if 'metricV40' in entry['impact']:
-                        cvss_version = '4.0'
-                        base_score = entry['impact']['metricV40']['baseScore']
-                        base_severity = entry['impact']['metricV40']['baseSeverity']
-                        base_vector = entry['impact']['metricV40']['vectorString']
-                    elif 'baseMetricV3' in entry['impact']:
-                        cvss_version = entry['impact']['baseMetricV3']['cvssV3']['version']
-                        base_score = entry['impact']['baseMetricV3']['cvssV3']['baseScore']
-                        base_severity = entry['impact']['baseMetricV3']['cvssV3']['baseSeverity']
-                        base_vector = entry['impact']['baseMetricV3']['cvssV3']['vectorString']
-                    else:
-                        cvss_version = entry['impact'].get('baseMetricV2', {}).get('cvssV2', {}).get('version', 'N/A')
-                        base_score = entry['impact'].get('baseMetricV2', {}).get('cvssV2', {}).get('baseScore', 'N/A')
-                        base_severity = entry['impact'].get('baseMetricV2', {}).get('severity', 'N/A')
-                        base_vector = entry['impact'].get('baseMetricV2', {}).get('cvssV2', {}).get('vectorString', 'N/A')
-                    assigner = entry['cve']['CVE_data_meta']['ASSIGNER']
-                    published_date = entry['publishedDate']
-                    description = entry['cve']['description']['description_data'][0]['value']
+                for entry in vulnerabilities:
+                    if not entry['cve']['description']['description_data'][0]['value'].startswith('**'):
+                        cve = entry['cve']['CVE_data_meta']['ID']
+                        if 'metricV40' in entry['impact']:
+                            cvss_version = '4.0'
+                            base_score = entry['impact']['metricV40']['baseScore']
+                            base_severity = entry['impact']['metricV40']['baseSeverity']
+                            base_vector = entry['impact']['metricV40']['vectorString']
+                        elif 'baseMetricV3' in entry['impact']:
+                            cvss_version = entry['impact']['baseMetricV3']['cvssV3']['version']
+                            base_score = entry['impact']['baseMetricV3']['cvssV3']['baseScore']
+                            base_severity = entry['impact']['baseMetricV3']['cvssV3']['baseSeverity']
+                            base_vector = entry['impact']['baseMetricV3']['cvssV3']['vectorString']
+                        else:
+                            cvss_version = entry['impact'].get('baseMetricV2', {}).get('cvssV2', {}).get('version', 'N/A')
+                            base_score = entry['impact'].get('baseMetricV2', {}).get('cvssV2', {}).get('baseScore', 'N/A')
+                            base_severity = entry['impact'].get('baseMetricV2', {}).get('severity', 'N/A')
+                            base_vector = entry['impact'].get('baseMetricV2', {}).get('cvssV2', {}).get('vectorString', 'N/A')
+                        assigner = entry['cve']['CVE_data_meta']['ASSIGNER']
+                        published_date = entry['publishedDate']
+                        description = entry['cve']['description']['description_data'][0]['value']
 
-                    dict_entry = {
-                        'cve': cve,
-                        'cvss_version': cvss_version,
-                        'base_score': base_score,
-                        'base_severity': base_severity,
-                        'base_vector': base_vector,
-                        'assigner': assigner,
-                        'published_date': published_date,
-                        'description': description
-                    }
-                    nvd_dict.extend([dict_entry])
+                        dict_entry = {
+                            'cve': cve,
+                            'cvss_version': cvss_version,
+                            'base_score': base_score,
+                            'base_severity': base_severity,
+                            'base_vector': base_vector,
+                            'assigner': assigner,
+                            'published_date': published_date,
+                            'description': description
+                        }
+                        nvd_dict.extend([dict_entry])
 
 
     nvd_df = pd.DataFrame(nvd_dict)
